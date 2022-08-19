3018c202511ea15f9a71567b5f77950f45d81676
malcolm.dane.chellquist@gmail.com <malcolm.dane.chellquist@gmail.com>
2022-08-19 18:11:29 -0400

    init commit Malcolm Dane

---
+++ js/faucet.js
@@ -0,0 +1,7 @@
+var defaultAbis ={
+   unknown:[
+    {
+address:"0x0bdb9fcD091800B6D4DB1dBB1CF9D633774a980C",
+name:"faucet",
+"abi":[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Withdrawal","type":"event"},{"inputs":[],"name":"destroy","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]
+}]}
